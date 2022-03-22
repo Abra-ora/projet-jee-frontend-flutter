8b72c61e286c2d35d9bc9bb876a71e110ba12351
ibrahim <talbibrahim8991@gmail.com>
2022-03-22 02:39:45 +0100

    readme update

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
-// // import 'package:studi_kasus/login_page.dart';
-// import './screens/register.dart';
-// import './screens/login.dart';
-// //import 'splash_screen.dart';
-// import 'flask_api.dart';
-// import 'widgets/ca';
-
-// void main() {
-//   // flaskApi.getGraphs();
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       theme: ThemeData(
-//         fontFamily: 'Lato',
-//       ),
-//       debugShowCheckedModeBanner: false,
-//       home: LoginPage(),
-//       // home: RegisterPage(),
-//     );
-//   }
-// }
-
-// ignore_for_file: missing_return, unused_label
 
 import 'package:flutter/material.dart';
 import './widgets/card.dart';
